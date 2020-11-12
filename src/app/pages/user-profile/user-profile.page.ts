38bb51822dd91fa4532795e714e0f0471557664f
Julius-Tabi <djudju00789@gmail.com>
2020-11-12 15:49:15 +0200

    redo15

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { from } from 'rxjs';
+import{FbserviceService} from '../../services/fbservice.service';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
-
-  constructor() { }
+  // ownerId: any;
+  // restaurants: Array<any>;
+  userProfile: any;
+  show = false;
+  
+  //content = 'addRest';
+  // array: any;
+  x: any
+  constructor(private fbservice: FbserviceService,private router: Router, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.fbservice.signAuth();
+    let user = firebase.auth().currentUser.uid
+    console.log('user: ', user)
+    
+    firebase.firestore().collection('Users').doc(user).get().then(snapshot => {
+      this.userProfile = snapshot.data();
+      //console.log('new data: ', this.restaurants)
+      // if(user === 'ownerId'){
+      //   this.show = this.restaurants
+      // }
+    })
   }
 
 }
